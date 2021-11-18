1c5849133031d5baafeaa0f41aa3908c9e2a20da
Nastia310104 <nastyuha-kotlyar@mail.ru>
2021-11-18 15:11:43 +1000

    db created. It seems my machine died. Thank you, Docker

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Books;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,13 +22,13 @@ class LibraryController extends AbstractController
      * @Route("/books", name="lib_books")
      */
     public function books(){
-        $years = ['13', '14', '15'];
+        //$years = ['13', '14', '15'];
         $filters = ['Name', 'Author', 'Year'];
         $option = 'foo'; //get from filters leter
         return $this->render('books.html.twig', [
             'title' => 'Books',
             'filters' => $filters,
-            'years' => $years,
+            //'years' => $years,
             'option' => $option
         ]);
     }
@@ -39,5 +40,38 @@ class LibraryController extends AbstractController
         return $this->render('authors.html.twig', [
             'title' => 'Authors'
         ]);
+    }
+
+    /**
+     * @Route("/new_book", name="lib_new_b")
+     */
+    public function add_new_book(){
+        $book = new Books();
+        $book->setBName('The Adventures of Oliver Twist')
+            ->setAuthor('Charles Dickens')
+            ->setYear('1839')
+            ->setImage('/public/images/no_image.jpg')
+            ->setDescription(<<<EOF
+
+            Dickens’ second novel, 
+            the book was originally published as a serial, in monthly 
+            instalments that began in February 1837 and continued until 
+            April 1839.
+            
+            Oliver Twist is the first novel in the English language 
+            to centre throughout on a child protagonist and is also 
+            notable for Dickens’ unromantic portrayal of criminals and 
+            their sordid lives.
+            
+            A mysterious pregnant woman is found lying in the street, 
+            and dies a few moments after giving birth to a son. The boy, 
+            Oliver Twist lives a life of poverty and misfortune in a workhouse. 
+            At the age of Nine, he escapes from the workhouse, only 
+            to find that greater dangers lurk outside.
+
+EOF
+            );
+        return new Response('let uus make some magic');
+        //return $this->render('addNew.html.twig');
     }
 }
---
+++ src/Entity/Books.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BooksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BooksRepository::class)
+ */
+class Books
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $b_name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBName(): ?string
+    {
+        return $this->b_name;
+    }
+
+    public function setBName(string $b_name): self
+    {
+        $this->b_name = $b_name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
